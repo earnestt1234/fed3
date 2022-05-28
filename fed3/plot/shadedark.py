d898c0f4b58ad3c35463e088d9a94a5c8bdc335e
Tom Earnest <earnestt1234@gmail.com>
2022-05-28 13:13:40 -0500

    add new lightcycle code / shade dark

--- fed3/plot/shadedark.py
+++ fed3/plot/shadedark.py
@@ -6,31 +6,9 @@ Created on Sun May  9 15:26:30 2021
 @author: earnestt1234
 """
 
-# from fed3.lightcycle import (hours_between, night_intervals)
+from fed3.lightcycle import night_intervals
 
-def is_at_night(datetime, lights_on, lights_off):
-
-    ashour = datetime.hour + (1/60) * datetime.minute
-    if lights_on > lights_off:
-        return (lights_off <= ashour < lights_on)
-    else:
-        return (ashour < lights_on or ashour >= lights_off)
-
-def nighttime_intervals(start_date, end_date, lights_on, lights_off):
-
-    result = []
-    pair = []
-
-    if is_at_night(start_date):
-        pair.append(start_date)
-    else:
-        first_night = start_date.replace(hour=lights_on)
-        first_night += pd.Timedelta('24H') * (first_night < start_date)
-        pair.append(first_night)
-
-
-def shade_darkness(ax, min_date, max_date, lights_on, lights_off,
-                   convert=True):
+def shade_darkness(ax, min_date, max_date, lights_on, lights_off):
     """
     Shade the night periods of a matplotlib Axes with a datetime x-axis.
 
@@ -46,25 +24,18 @@ def shade_darkness(ax, min_date, max_date, lights_on, lights_off,
         Integer between 0 and 23 representing when the light cycle begins.
     lights_off : int
         Integer between 0 and 23 representing when the light cycle ends.
-    convert : bool, optional
-        Whether to convert the start/end arguments from numpy datetime to
-        standard datetime. The default is True.
 
     Returns
     -------
     None.
     """
-    hours_list = hours_between(min_date, max_date,convert=convert)
-    nights = night_intervals(hours_list, lights_on=lights_on,
-                             lights_off=lights_off)
-    if nights:
-        for i, interval in enumerate(nights):
-            start = interval[0]
-            end = interval[1]
-            if start != end:
-                ax.axvspan(start,
-                           end,
-                           color='gray',
-                           alpha=.2,
-                           label='_'*i + 'lights off',
-                           zorder=0)
+
+    nights = night_intervals(min_date, max_date, lights_on, lights_off)
+
+    for i, (start, end) in enumerate(nights):
+        ax.axvspan(start,
+                   end,
+                   color='gray',
+                   alpha=.2,
+                   label='_'*i + 'lights off',
+                   zorder=0)
