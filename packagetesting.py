ccf376805ea448c08810812774dc951ed060d5d2
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2022-05-30 18:11:16 -0500

    continue testing bar

--- packagetesting.py
+++ packagetesting.py
@@ -12,8 +12,11 @@ import fed3.plot as fplot
 import numpy as np
 
 # load FED data
-a = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED7Cat.csv")
-b = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED3Cat.csv")
+# a = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED7Cat.csv")
+# b = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED3Cat.csv")
+
+a = fed3.load(r"C:\Users\earne\Documents\fedviz\justin_data\FED3Cat.csv")
+b = fed3.load(r"C:\Users\earne\Documents\fedviz\justin_data\FED7Cat.csv")
 
 
 from fed3.plot.helpers import _parse_feds
@@ -23,7 +26,7 @@ from fed3.metrics.tables import (_create_group_metric_df,  _create_metric_df, _b
 
 from fed3.metrics.core import (_get_metric, _get_metricname,)
 
-feds = {'a':[a, b], 'b':a}
+feds = {'a':[a, b], 'b':[a, b]}
 y = 'pellets'
 stat = 'max'
 mixed_align = 'raise'
@@ -51,4 +54,5 @@ errors = DATA.iloc[:, -1]
 idx = range(len(DATA))
 
 fig, ax = plt.subplots()
-ax.bar(idx, barvals)
+ax.bar(idx, barvals, label=DATA.index)
+ax.errorbar(x=idx, y=barvals, yerr=errors, color='grey', ls='none', capsize=5)
