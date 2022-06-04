d5290474ff1ae8ac523c2e2dbe03be7478fadf94
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2022-06-03 22:58:55 -0500

    add positioning of bars

--- packagetesting.py
+++ packagetesting.py
@@ -26,10 +26,39 @@ from fed3.metrics.tables import (_create_group_metric_df,  _create_metric_df, _b
 
 from fed3.metrics.core import (_get_metric, _get_metricname,)
 
-feds = {'a':[a, b], 'b':[a, b]}
+def assign_bar_positions_widths(feds_dict, positions_arg=None, position_width=0.75):
+
+    if positions_arg is None:
+        positions_arg = np.arange(len(feds_dict))
+
+    if len(positions_arg) != len(feds_dict):
+        raise ValueError('positions argument must equal the length of the input')
+
+    positions = np.array(positions_arg, dtype='int')
+
+    if any(np.diff(positions) > 1):
+        raise ValueError("positions must be only integers with none skipped.")
+
+    centers, counts = np.unique(positions, return_counts=True)
+    bw = position_width / max(counts)
+
+    positions = []
+    bar_widths = []
+    for x, c in zip(centers, counts):
+        start = x - (.5 * c * bw)
+        end  = x + (.5 * c * bw)
+        positions += list(np.arange(start + bw/2, end, bw))
+        bar_widths += [bw] * c
+
+    return centers, positions, bar_widths
+
+feds = {'a':[a, b], 'b':[a, b],
+        'c':[a, b], 'd':[a, b]}
 y = 'pellets'
 stat = 'max'
 mixed_align = 'raise'
+positions = [0, 0, 1, 1]
+position_labels = ['A', 'B']
 
 import matplotlib.pyplot as plt
 
@@ -51,8 +80,23 @@ DATA = _bar_metric_df(feds_dict, metric, stat)
 individual_data = DATA.iloc[:, :-2]
 barvals = DATA.iloc[:, -2]
 errors = DATA.iloc[:, -1]
-idx = range(len(DATA))
+
+position_width = 0.75
+centers, positions, bar_widths = assign_bar_positions_widths(feds,
+                                                             positions,
+                                                             position_width=position_width)
 
 fig, ax = plt.subplots()
-ax.bar(idx, barvals, label=DATA.index)
-ax.errorbar(x=idx, y=barvals, yerr=errors, color='grey', ls='none', capsize=5)
+
+for i, x in enumerate(positions):
+    y = barvals[i]
+    w = bar_widths[i]
+    label = DATA.index[i]
+    err = errors[i]
+
+    ax.bar(x, y, width=w, label=label)
+    ax.errorbar(x=x, y=y, yerr=err, color='grey', ls='none', capsize=5)
+
+ax.set_xticks(centers)
+if position_labels is not None:
+    ax.set_xticklabels(position_labels)
