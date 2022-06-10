973152da955a7414c8b370e0a6b2dcb463611e32
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2022-06-09 22:08:41 -0500

    add correct / error metrics

--- fed3/metrics/core.py
+++ fed3/metrics/core.py
@@ -129,12 +129,12 @@ def cumulative_right_pokes(fed, bins=None, origin='start'):
     agg = 'max'
     return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
 
-def cumulative_left_percentage(fed, bins=None, origin='start'):
+def cumulative_left_percent(fed, bins=None, origin='start'):
     func = lambda f: _cumulative_poke_percentage_general(f, 'left')
     agg = 'last'
     return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
 
-def cumulative_right_percentage(fed, bins=None, origin='start'):
+def cumulative_right_percent(fed, bins=None, origin='start'):
     func = lambda f: _cumulative_poke_percentage_general(f, 'right')
     agg = 'last'
     return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
@@ -145,6 +145,48 @@ def left_pokes(fed, bins=None, origin='start'):
 
 def right_pokes(fed, bins=None, origin='start'):
     func = cumulative_right_pokes if bins is None else binary_right_pokes
+    return func(fed, bins=bins, origin=origin)
+
+# ---- Correct / Error pokes
+
+# you can do a find and replace on the left/right ...
+
+def binary_correct_pokes(fed, bins=None, origin='start'):
+    func = lambda f: f.pokes(kind='correct', cumulative=False, condense=True)
+    agg = 'sum'
+    return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
+
+def binary_error_pokes(fed, bins=None, origin='start'):
+    func = lambda f: f.pokes(kind='error', cumulative=False, condense=True)
+    agg = 'sum'
+    return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
+
+def cumulative_correct_pokes(fed, bins=None, origin='start'):
+    func = lambda f: f.pokes(kind='correct', cumulative=True, condense=True)
+    agg = 'max'
+    return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
+
+def cumulative_error_pokes(fed, bins=None, origin='start'):
+    func = lambda f: f.pokes(kind='error', cumulative=True, condense=True)
+    agg = 'max'
+    return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
+
+def cumulative_correct_percent(fed, bins=None, origin='start'):
+    func = lambda f: _cumulative_poke_percentage_general(f, 'correct')
+    agg = 'last'
+    return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
+
+def cumulative_error_percent(fed, bins=None, origin='start'):
+    func = lambda f: _cumulative_poke_percentage_general(f, 'error')
+    agg = 'last'
+    return _default_metric(fed=fed, func=func, bins=bins, origin=origin, agg=agg)
+
+def correct_pokes(fed, bins=None, origin='start'):
+    func = cumulative_correct_pokes if bins is None else binary_correct_pokes
+    return func(fed, bins=bins, origin=origin)
+
+def error_pokes(fed, bins=None, origin='start'):
+    func = cumulative_error_pokes if bins is None else binary_error_pokes
     return func(fed, bins=bins, origin=origin)
 
 # ---- Other
@@ -190,21 +232,29 @@ def get_metric(y, kind=None):
 # link keywords to their default function
 Metric = namedtuple("Metric", ['func', 'nicename'])
 
-METRICS = {'binary_pellets'           : Metric(binary_pellets, "Pellets"),
-           'cumulative_pellets'       : Metric(cumulative_pellets, "Pellets"),
-           'pellets'                  : Metric(pellets, "Pellets"),
-           'binary_pokes'             : Metric(binary_pokes, "Pokes"),
-           'cumulative_pokes'         : Metric(cumulative_pokes, "Pokes"),
-           'pokes'                    : Metric(pokes, "Pokes"),
-           'binary_left_pokes'        : Metric(binary_left_pokes, "Left Pokes"),
-           'binary_right_pokes'       : Metric(binary_left_pokes, "Right Pokes"),
-           'cumulative_left_pokes'    : Metric(binary_left_pokes, "Left Pokes"),
-           'cumulative_right_pokes'   : Metric(binary_left_pokes, "Right Pokes"),
-           'cumulative_left_percent'  : Metric(cumulative_left_percentage, "Left Pokes (%)"),
-           'cumulative_right_percent' : Metric(cumulative_right_percentage, "Right Pokes (%)"),
-           'left_pokes'               : Metric(left_pokes, "Left Pokes"),
-           'right_pokes'              : Metric(right_pokes, "Right Pokes"),
-           'battery'                  : Metric(battery, "Battery Life (V)"),
-           'ipi'                      : Metric(ipi, "Interpellet Intervals"),
-           'motor'                    : Metric(motor_turns, "Motor Turns"),
-           'rt'                       : Metric(retrival_time, "Retrieval Time (s)")}
+METRICS = {'binary_pellets'             : Metric(binary_pellets, "Pellets"),
+           'cumulative_pellets'         : Metric(cumulative_pellets, "Pellets"),
+           'pellets'                    : Metric(pellets, "Pellets"),
+           'binary_pokes'               : Metric(binary_pokes, "Pokes"),
+           'cumulative_pokes'           : Metric(cumulative_pokes, "Pokes"),
+           'pokes'                      : Metric(pokes, "Pokes"),
+           'binary_left_pokes'          : Metric(binary_left_pokes, "Left Pokes"),
+           'binary_right_pokes'         : Metric(binary_right_pokes, "Right Pokes"),
+           'cumulative_left_pokes'      : Metric(cumulative_left_pokes, "Left Pokes"),
+           'cumulative_right_pokes'     : Metric(cumulative_right_pokes, "Right Pokes"),
+           'cumulative_left_percent'    : Metric(cumulative_left_percent, "Left Pokes (%)"),
+           'cumulative_right_percent'   : Metric(cumulative_right_percent, "Right Pokes (%)"),
+           'left_pokes'                 : Metric(left_pokes, "Left Pokes"),
+           'right_pokes'                : Metric(right_pokes, "Right Pokes"),
+           'binary_correct_pokes'       : Metric(binary_correct_pokes, "correct Pokes"),
+           'binary_error_pokes'         : Metric(binary_error_pokes, "Right Pokes"),
+           'cumulative_correct_pokes'   : Metric(cumulative_correct_pokes, "Left Pokes"),
+           'cumulative_error_pokes'     : Metric(cumulative_error_pokes, "Right Pokes"),
+           'cumulative_correct_percent' : Metric(cumulative_correct_percent, "Left Pokes (%)"),
+           'cumulative_error_percent'   : Metric(cumulative_error_percent, "Right Pokes (%)"),
+           'correct_pokes'              : Metric(correct_pokes, "Left Pokes"),
+           'error_pokes'                : Metric(error_pokes, "Right Pokes"),
+           'battery'                    : Metric(battery, "Battery Life (V)"),
+           'ipi'                        : Metric(ipi, "Interpellet Intervals"),
+           'motor'                      : Metric(motor_turns, "Motor Turns"),
+           'rt'                         : Metric(retrival_time, "Retrieval Time (s)")}
--- packagetesting.py
+++ packagetesting.py
@@ -9,15 +9,13 @@ Created on Sat May  1 11:02:24 2021
 import fed3
 import fed3.plot as fplot
 
-import numpy as np
-
 # load FED data
-a = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED7Cat.csv")
-b = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED3Cat.csv")
+# a = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED7Cat.csv")
+# b = fed3.load(r"/Users/earnestt1234/Documents/fedviz/justin_data/FED3Cat.csv")
 
-# a = fed3.load(r"C:\Users\earne\Documents\fedviz\justin_data\FED3Cat.csv")
+a = fed3.load(r"C:\Users\earne\Documents\fedviz\justin_data\FED3Cat.csv")
 # b = fed3.load(r"C:\Users\earne\Documents\fedviz\justin_data\FED7Cat.csv")
 # c = fed3.load(r"C:\Users\earne\Documents\fedviz\WEEK 1\WEEK 1 - Copy\4-1_FED3024_111120_01.CSV")
 # d = fed3.load(r"C:\Users\earne\Documents\fedviz\poketime\FED002_110920_02.CSV")
 
-fplot.line(a, y='pellets')
+fplot.scatter(a, y='ipi')
